bdfd294d496856ba73dc77df275b39894c9ccb7b
Jackson Ming Hu <huming2207@gmail.com>
2020-03-04 15:57:35 +1100

    Switch to mongodb for now

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,25 @@
-use actix_web::{HttpServer, Responder, web};
+#[macro_use]
+extern crate actix_web;
+#[macro_use]
+extern crate log;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate serde_json;
+extern crate actix_rt;
+extern crate serde;
+extern crate dotenv;
+extern crate jsonwebtoken;
+extern crate uuid;
+extern crate bcrypt;
+
+pub mod model;
+
+use std::env;
+use actix_web::{get, App, HttpServer, Responder, web};
+use r2d2_mongodb::{MongodbConnectionManager, ConnectionOptions};
+use r2d2::Pool;
+
 
 #[get("/{id}/{name}/index.html")]
 async fn index(info: web::Path<(u32, String)>) -> impl Responder {
@@ -7,6 +28,17 @@ async fn index(info: web::Path<(u32, String)>) -> impl Responder {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    let dbManager = MongodbConnectionManager::new(
+    ConnectionOptions::builder()
+                .with_host(env::var("MONGO_HOST").unwrap().as_str(),
+                           env::var("MONGO_PORT").unwrap().parse::<u16>().unwrap())
+                .with_db(env::var("MONGO_DBNAME").unwrap().as_str()).build()
+
+    );
+
+    let pool = Pool::builder().max_size(10).build(manager).unwrap();
+
     HttpServer::new(|| App::new().service(index))
         .bind("127.0.0.1:8080")?
         .run()
--- src/model/user.rs
+++ src/model/user.rs
@@ -1,28 +1,9 @@
-#[macro_use]
-extern crate diesel;
+use bson;
 
-use std::time::SystemTime;
-
-#[cfg(test)]
-use diesel::debug_query;
-#[cfg(test)]
-use diesel::pg::Pg;
-use diesel::prelude::*;
-
-
-table! {
-    users {
-        id -> Integer,
-        username -> Text,
-        email -> Text,
-        password -> Text,
-        bio -> Text
-    }
-}
-
-#[derive(Queryable, Identifiable, AsChangeset)]
+#[derive(Serialize, Deserialize)]
 pub struct User {
-    pub id: u64,
+    #[serde(rename = "_id")]
+    pub id: bson::oid::ObjectId,
     pub username: String,
     pub email: String,
     pub password: String,
@@ -31,6 +12,7 @@ pub struct User {
 
 impl User {
     pub fn find_user_with_username(username: &str, conn: &dyn Connection) -> QueryResult<User> {
+
     }
 }
 
