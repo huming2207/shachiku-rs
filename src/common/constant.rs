b9558b80bd725957d5b7ceda33fff66524bd8c44
Jackson Ming Hu <huming2207@gmail.com>
2020-03-09 10:25:27 +1100

    model: add user

---
+++ src/common/constant.rs
@@ -0,0 +1,6 @@
+pub const EMPTY: &str = "";
+
+// dotenv config keys
+pub const MONGO_DB_NAME: &str = "MONGO_DB_NAME";
+pub const MONGO_DB_HOST: &str = "MONGO_DB_HOST";
+pub const PASSWORD_SALT: &str = "PASSWORD_SALT";
--- src/model/user.rs
+++ src/model/user.rs
@@ -1,4 +1,8 @@
 use bson;
+use serde::{Deserialize, Serialize};
+use argon2::{self, Config};
+use std::env;
+use crate::constant;
 
 #[derive(Serialize, Deserialize)]
 pub struct User {
@@ -11,4 +15,16 @@ pub struct User {
     pub bio: String,
 }
 
+impl User {
+    pub fn set_password(&mut self, raw_passwd: &str) {
+        let salt = env::var(constant::PASSWORD_SALT).unwrap().as_str();
+        let config = Config::default();
+        self.password = argon2::hash_encoded(raw_passwd, salt, &config).unwrap();
+    }
 
+    pub fn compare_password(&mut self, raw_passwd: &str) -> bool {
+        let salt = env::var(constant::PASSWORD_SALT).unwrap().as_str();
+        let config = Config::default();
+        return argon2::verify_encoded(self.password.to_str(), raw_passwd).unwrap();
+    }
+}
