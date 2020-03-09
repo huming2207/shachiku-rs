0f5eca9f053bc4b35f7be888bc9ee07f7c6585c1
Jackson Ming Hu <huming2207@gmail.com>
2020-03-09 15:20:26 +1100

    constant: add more constant values for controllers

--- src/common/constant.rs
+++ src/common/constant.rs
@@ -4,3 +4,8 @@ pub const EMPTY: &str = "";
 pub const MONGO_DB_NAME: &str = "MONGO_DB_NAME";
 pub const MONGO_DB_HOST: &str = "MONGO_DB_HOST";
 pub const PASSWORD_SALT: &str = "PASSWORD_SALT";
+
+// MongoDB collection
+pub const MONGO_COLL_USER: &str = "user";
+
+pub const JSON_CONTENT: &str = "application/json";
