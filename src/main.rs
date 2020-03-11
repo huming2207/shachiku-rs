f84ffd59dc1697d09bc9c5119042cf438a1cd336
Jackson Ming Hu <huming2207@gmail.com>
2020-03-11 21:47:12 +1100

    main: add logger middleware

--- src/main.rs
+++ src/main.rs
@@ -26,15 +26,21 @@ use std::env;
 use actix_web::{get, App, HttpServer, Responder, web};
 use mongodb::{Client, options::ClientOptions};
 use common::constant;
+use actix_web::middleware::Logger;
+use log::Level;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
     dotenv::dotenv().ok();
     let db_option = ClientOptions::parse(env::var(constant::MONGO_DB_HOST).unwrap().as_str()).unwrap();
     let db_client = Client::with_options(db_option).unwrap();
     let db_database = db_client.database(env::var(constant::MONGO_DB_NAME).unwrap().as_str());
 
-    HttpServer::new(move || App::new().data(db_database.clone()).configure(app::load_services))
+    HttpServer::new(move || App::new()
+        .wrap(Logger::default())
+        .data(db_database.clone()).configure(app::load_services))
         .bind(env::var(constant::LISTEN_ADDR).unwrap().as_str())?
         .run()
         .await
